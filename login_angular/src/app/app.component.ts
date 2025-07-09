47e0247b0546230c158acdf3e2d2f3fb5e832720
maicol <michael.borja@gmail.com>
2025-07-08 20:34:03 -0500

    Editado título del proyecto en app.component.ts

--- login_angular/src/app/app.component.ts
+++ login_angular/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'login_angular';
+  title = 'Login Angular con Material Design';
 }
