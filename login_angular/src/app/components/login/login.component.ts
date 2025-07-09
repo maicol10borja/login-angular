eb3af77b5f99a797ed384ec6c7f6fd74ec333f77
maicol <michael.borja@gmail.com>
2025-07-08 21:13:37 -0500

    Login funcional con Angular Material

---
+++ login_angular/src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+// Importa estos módulos de Angular Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common'; // Importa CommonModule para usar directivas básicas
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']  // corregí aquí styleUrls (estaba mal escrito)
+})
+export class LoginComponent {}
